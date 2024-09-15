5eeb699b6bef94223e8216b94b7637251254f85c
Fr4nc021 <164265892+Fr4nc021@users.noreply.github.com>
2024-09-15 16:34:51 -0300

    feature: add initial layout to currentweather

--- .history/frontend/clima-360/src/components/CurrentWeather/index_20240915162352.jsx
+++ .history/frontend/clima-360/src/components/CurrentWeather/index_20240915162352.jsx
@@ -3,12 +3,9 @@ export default function CurrentWeather() {
     <> <div>
 <img src="#" alt="Sol" />
       <h1>Ensolarado</h1>
-      <img src="#" alt="Local atual" />
     <h2>Caxias do Sul</h2>
     <span>28</span>
-    <img src="#" alt="maxima" />
     <span>15</span>
-    <img src="#" alt="minima" />
     <span>23C°</span>
     <p>Domingo, 15 Set 2024</p>  
 
