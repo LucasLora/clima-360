03bb95f781315007d5feaad0640319142c2a00cd
Lucas Lora <78751573+LucasLora@users.noreply.github.com>
2024-11-20 11:20:47 -0300

    Rework Forecast component and add styles

--- frontend/clima-360/src/components/Forecast/index.jsx
+++ frontend/clima-360/src/components/Forecast/index.jsx
@@ -1,33 +1,64 @@
+import "./Forecast.css";
+
 export default function Forecast() {
   return (
-    <>
-      <div>
-        <p>Seg. Chuva</p>
-        <p>15c°</p>
-        <img src="#" alt="minima" />
-        <p>28c°</p>
-        <img src="#" alt="maxima" />
-        <p>Ter. Sol</p>
-        <p>19c°</p>
-        <img src="#" alt="minima" />
-        <p>30c°</p>
-        <img src="#" alt="maxima" />
-        <p>Qua. Nublado</p>
-        <p>18c°</p>
-        <img src="#" alt="minima" />
-        <p>29c°</p>
-        <img src="#" alt="maxima" />
-        <p>Qui. Tempestade</p>
-        <p>13c°</p>
-        <img src="#" alt="minima" />
-        <p>17c°</p>
-        <img src="#" alt="maxima" />
-        <p>Sex. Neve</p>
-        <p>7c°</p>
-        <img src="#" alt="minima" />
-        <p>12c°</p>
-        <img src="#" alt="maxima" />
+    <div className="forecast-container">
+      <p>Próximos 5 dias</p>
+
+      <div className="row">
+        <img src="logo.png" alt="chuva" className="weather-icon" />
+        <span className="day">Seg. Chuva</span>
+        <div className="temperature-details">
+          <img src="down.png" alt="minima" className="arrow-img" />
+          <span>28<span className="degree-symbol">°C</span></span>
+          <img src="up.png" alt="maxima" className="arrow-img" />
+          <span>30<span className="degree-symbol">°C</span></span>
+        </div>
       </div>
-    </>
+
+      <div className="row">
+        <img src="logo.png" alt="sol" className="weather-icon" />
+        <span className="day">Ter. Sol</span>
+        <div className="temperature-details">
+          <img src="down.png" alt="minima" className="arrow-img" />
+          <span>30<span className="degree-symbol">°C</span></span>
+          <img src="up.png" alt="maxima" className="arrow-img" />
+          <span>32<span className="degree-symbol">°C</span></span>
+        </div>
+      </div>
+
+      <div className="row">
+        <img src="logo.png" alt="nublado" className="weather-icon" />
+        <span className="day">Qua. Nublado</span>
+        <div className="temperature-details">
+          <img src="down.png" alt="minima" className="arrow-img" />
+          <span>22<span className="degree-symbol">°C</span></span>
+          <img src="up.png" alt="maxima" className="arrow-img" />
+          <span>26<span className="degree-symbol">°C</span></span>
+        </div>
+      </div>
+
+      <div className="row">
+        <img src="logo.png" alt="tempestade" className="weather-icon" />
+        <span className="day">Qui. Tempestade</span>
+        <div className="temperature-details">
+          <img src="down.png" alt="minima" className="arrow-img" />
+          <span>17<span className="degree-symbol">°C</span></span>
+          <img src="up.png" alt="maxima" className="arrow-img" />
+          <span>20<span className="degree-symbol">°C</span></span>
+        </div>
+      </div>
+
+      <div className="row">
+        <img src="logo.png" alt="neve" className="weather-icon" />
+        <span className="day">Sex. Neve</span>
+        <div className="temperature-details">
+          <img src="down.png" alt="minima" className="arrow-img" />
+          <span>10<span className="degree-symbol">°C</span></span>
+          <img src="up.png" alt="maxima" className="arrow-img" />
+          <span>12<span className="degree-symbol">°C</span></span>
+        </div>
+      </div>
+    </div>
   );
 }
