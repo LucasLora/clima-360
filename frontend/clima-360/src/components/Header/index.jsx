b21e8c296bf8e51ced60d3828261509917029493
Lucas Lora <78751573+LucasLora@users.noreply.github.com>
2024-11-24 22:27:13 -0300

    Add dots menu example to header component

--- frontend/clima-360/src/components/Header/index.jsx
+++ frontend/clima-360/src/components/Header/index.jsx
@@ -5,6 +5,11 @@ export default function Header() {
     <header className="header">
       <img src="logo.png" alt="Sol" width={25} />
       <input type="text" placeholder="Pesquisar local" className="search-bar" />
+      <div className="menu-dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </header>
   );
 }
